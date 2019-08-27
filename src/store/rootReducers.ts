b66c7a60c53a62174216979b88827b0130c8a529
juztinlazaro <juztinlazaro@gmail.com>
2019-08-28 06:12:30 +0800

    changes: resolve path of home/reducer

--- src/store/rootReducers.ts
+++ src/store/rootReducers.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 import { IRootReducer } from './rootReducer.interface';
-import homeReducer from './home/reducer';
+import homeReducer from './modules/home/reducer';
 
 const rootReducers = combineReducers<IRootReducer>({
   homes: homeReducer,
